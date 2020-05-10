e9243282cacddabdd9b9a8be2a5579d454c0beb4
rakshith15717 <61398045+rakshith15717@users.noreply.github.com>
2020-05-10 17:36:20 +1000

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -11,6 +11,6 @@ for line in sys.stdin:
      line = line.strip()
      words = line.split()
      for word in words:
-         print ('%s\t%s' % (word, "1"))
+         print '%s\t%s' % (word, "1")
 
 
